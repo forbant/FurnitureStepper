7bd3aec51949e567608a53282c216506f326fd1d
forbant <45597703+forbant@users.noreply.github.com>
2023-09-14 22:47:12 +0300

    Create calibratinon.cpp(backup file)

---
+++ src/calibratinon.cpp
@@ -0,0 +1,53 @@
+const int dirPin = 2;
+const int stepPin = 3;
+const int enablePin = 13;
+const int stepsPerRevolution = 200;
+int delayTime = 640;
+
+int buttonFwd = 6;
+int buttonBack = 8;
+
+void setup()
+{
+    pinMode(stepPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+    pinMode(enablePin, OUTPUT);
+
+    pinMode(buttonFwd, INPUT_PULLUP);
+    pinMode(buttonBack, INPUT_PULLUP);
+    Serial.begin(9600);
+}
+void move()
+{
+    for (int x = 0; x < stepsPerRevolution; x++)
+    {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(delayTime);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(delayTime);
+    }
+    Serial.println("END");
+}
+void loop()
+{
+    bool f = !digitalRead(buttonFwd);
+    bool b = !digitalRead(buttonBack);
+    if (f)
+    {
+        digitalWrite(enablePin, LOW);
+        Serial.println("FORWARD");
+        digitalWrite(dirPin, HIGH);
+        move();
+    }
+    if (b)
+    {
+        digitalWrite(enablePin, LOW);
+        Serial.println("BACKWARD");
+        digitalWrite(dirPin, LOW);
+        move();
+    }
+    if (!f && !b)
+    {
+        digitalWrite(enablePin, HIGH);
+    }
+}
