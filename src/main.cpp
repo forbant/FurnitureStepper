90a2908224d2deb89a0ccfed8815b2afceedd770
Andrii Tiutiakh <andrii.tiutiakh@globallogic.com>
2023-09-29 18:28:49 +0300

    v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
-#define DEBUG_MSG // ЗАКОМЕНТУЙ ЦЕЙ РЯДОК ЯКЩО ВСЕ ПРАЦЮЄ
-
-#define STEP_PIN 1      // ПІН STEP
-#define DIR_PIN 2       // ПІН DIR
-#define ENABLE_PIN 3    // ПІН EN
+#define STEP_PIN 3      // ПІН STEP
+#define DIR_PIN 2      // ПІН DIR
+#define ENABLE_PIN 13    // ПІН EN
 #define STOPPER_PIN_1 4 //  КІНЦЕВИК 1
 #define STOPPER_PIN_2 5 //  КІНЦЕВИК 2
 
 #define fwd_button 6 // КНОПКА ВПЕРЕД
-#define rev_button 7 // КНОПКА НАЗАД
-
-// Кількість кроків назад коли впирається в кінцевик
-#define REVERSE_STEPS 100
+#define rev_button 8 // КНОПКА НАЗАД
 
 /*
     Час між імпульсами у мксек. 1000 мксек = 1 мсек = 0.001 сек. Частота проходження імпульсів 1/0.001 = 1 кГц,
@@ -50,9 +45,6 @@ void stopMotor();
 
 void setup()
 {
-    #ifdef DEBUG_MSG
-        Serial.begin(9600);
-    #endif
     pinMode(fwd_button, INPUT_PULLUP);
     pinMode(rev_button, INPUT_PULLUP);
     pinMode(STOPPER_PIN_1, INPUT_PULLUP);
@@ -64,6 +56,7 @@ void setup()
     digitalWrite(ENABLE_PIN, HIGH);
 
     state = STANDBY;
+    isMotorRunning = false;
 }
 
 void loop()
@@ -94,9 +87,6 @@ void loop()
         }
         break;
     default:
-        #ifdef DEBUG_MSG
-            Serial.println("Oops");
-        #endif
         break;
     }
 }
@@ -113,17 +103,9 @@ void readButtons()
 {
     stopper1 = !digitalRead(STOPPER_PIN_1);
     stopper2 = !digitalRead(STOPPER_PIN_2);
-#ifdef DEBUG_MSG
-    Serial.print("stopper1:"); Serial.println(stopper1);
-    Serial.print("stopper2:"); Serial.println(stopper2);
-#endif
 
     forwardButton = !digitalRead(fwd_button);
     backwardButton = !digitalRead(rev_button);
-#ifdef DEBUG_MSG
-    Serial.print("forwardButton:"); Serial.println(forwardButton);
-    Serial.print("backwardButton:"); Serial.println(backwardButton);
-#endif
 }
 
 void handleState()
@@ -133,20 +115,19 @@ void handleState()
         state = FORWARD;
         digitalWrite(DIR_PIN, Direction::Forward);
         isMotorRunning = true;
+        digitalWrite(ENABLE_PIN, LOW);
     }
     if (!isMotorRunning && !stopper2 && backwardButton)
     {
         state = BACKWARD;
         digitalWrite(DIR_PIN, Direction::Backward);
         isMotorRunning = true;
+        digitalWrite(ENABLE_PIN, LOW);
     }
 }
 
 void stopMotor()
 {
-    #ifdef DEBUG_MSG
-        Serial.println("stopMotor");
-    #endif
     state = STANDBY;
     isMotorRunning = false;
     digitalWrite(ENABLE_PIN, HIGH);
