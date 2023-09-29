fbcb57d5c3416e58c601c5b3294415a9985ebc49
Andrii Tiutiakh <andrii.tiutiakh@globallogic.com>
2023-09-29 13:26:07 +0300

    Change the way we handle buttons

--- src/calibratinon.cpp
+++ src/calibratinon.cpp
@@ -1,53 +1,56 @@
-const int dirPin = 2;
-const int stepPin = 3;
-const int enablePin = 13;
-const int stepsPerRevolution = 200;
-int delayTime = 640;
+// ЦЕЙ КОД ТІЛЬКИ ДЛЯ КАЛІБРОВКИ
+// #include "Arduino.h"
 
-int buttonFwd = 6;
-int buttonBack = 8;
+// const int dirPin = 2;
+// const int stepPin = 3;
+// const int enablePin = 13;
+// const int stepsPerRevolution = 200;
+// int delayTime = 640;
 
-void setup()
-{
-    pinMode(stepPin, OUTPUT);
-    pinMode(dirPin, OUTPUT);
-    pinMode(enablePin, OUTPUT);
+// int buttonFwd = 6;
+// int buttonBack = 8;
 
-    pinMode(buttonFwd, INPUT_PULLUP);
-    pinMode(buttonBack, INPUT_PULLUP);
-    Serial.begin(9600);
-}
-void move()
-{
-    for (int x = 0; x < stepsPerRevolution; x++)
-    {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(delayTime);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(delayTime);
-    }
-    Serial.println("END");
-}
-void loop()
-{
-    bool f = !digitalRead(buttonFwd);
-    bool b = !digitalRead(buttonBack);
-    if (f)
-    {
-        digitalWrite(enablePin, LOW);
-        Serial.println("FORWARD");
-        digitalWrite(dirPin, HIGH);
-        move();
-    }
-    if (b)
-    {
-        digitalWrite(enablePin, LOW);
-        Serial.println("BACKWARD");
-        digitalWrite(dirPin, LOW);
-        move();
-    }
-    if (!f && !b)
-    {
-        digitalWrite(enablePin, HIGH);
-    }
-}
+// void setup()
+// {
+//     pinMode(stepPin, OUTPUT);
+//     pinMode(dirPin, OUTPUT);
+//     pinMode(enablePin, OUTPUT);
+
+//     pinMode(buttonFwd, INPUT_PULLUP);
+//     pinMode(buttonBack, INPUT_PULLUP);
+//     Serial.begin(9600);
+// }
+// void move()
+// {
+//     for (int x = 0; x < stepsPerRevolution; x++)
+//     {
+//         digitalWrite(stepPin, HIGH);
+//         delayMicroseconds(delayTime);
+//         digitalWrite(stepPin, LOW);
+//         delayMicroseconds(delayTime);
+//     }
+//     Serial.println("END");
+// }
+// void loop()
+// {
+//     bool f = !digitalRead(buttonFwd);
+//     bool b = !digitalRead(buttonBack);
+//     if (f)
+//     {
+//         digitalWrite(enablePin, LOW);
+//         Serial.println("FORWARD");
+//         digitalWrite(dirPin, HIGH);
+//         move();
+//     }
+//     if (b)
+//     {
+//         digitalWrite(enablePin, LOW);
+//         Serial.println("BACKWARD");
+//         digitalWrite(dirPin, LOW);
+//         move();
+//     }
+//     if (!f && !b)
+//     {
+//         digitalWrite(enablePin, HIGH);
+//     }
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,43 @@
 #include <Arduino.h>
 
-#define STEP_PIN 2
-#define DIR_PIN 3
-#define ENABLE_PIN 4
-#define STOPPER_PIN 8
+#define DEBUG_MSG // ЗАКОМЕНТУЙ ЦЕЙ РЯДОК ЯКЩО ВСЕ ПРАЦЮЄ
 
-#define fwd_button 6
-#define rev_button 7
+#define STEP_PIN 1      // ПІН STEP
+#define DIR_PIN 2       // ПІН DIR
+#define ENABLE_PIN 3    // ПІН EN
+#define STOPPER_PIN_1 4 //  КІНЦЕВИК 1
+#define STOPPER_PIN_2 5 //  КІНЦЕВИК 2
+
+#define fwd_button 6 // КНОПКА ВПЕРЕД
+#define rev_button 7 // КНОПКА НАЗАД
 
 // Кількість кроків назад коли впирається в кінцевик
 #define REVERSE_STEPS 100
 
 /*
     Час між імпульсами у мксек. 1000 мксек = 1 мсек = 0.001 сек. Частота проходження імпульсів 1/0.001 = 1 кГц,
-     Не рекомендується встановлювати час менше 100 мсек, оскільки частота буде 10 кГц
+    Не рекомендується встановлювати час менше 100 мсек, оскільки частота буде 10 кГц
 */
 #define frequency 640
 
-// Скільки мс треба тримати щоб перейшло в режим ручного керування 1000мс = 1с
-#define holdThreshold 1000
-
-int timer = 0;
-int buttonHold = 0;
 bool isMotorRunning = false;
 
-bool stopper = false;
+bool stopper1 = false;
+bool stopper2 = false;
 bool forwardButton = false;
 bool backwardButton = false;
 
-enum Direction {
+enum Direction
+{
     Forward = 0x1,
     Backward = 0x0
 };
 
-enum State {
+enum State
+{
     STANDBY,
-    MANUAL_MOVE,
-    AUTO_MOVE,
-    REVERSE
+    FORWARD,
+    BACKWARD
 };
 
 State state;
@@ -45,109 +45,109 @@ Direction direction = Forward;
 
 void move();
 void readButtons();
-void reverse();
 void handleState();
 void stopMotor();
 
-void setup() {
-    Serial.begin(9600);
+void setup()
+{
+    #ifdef DEBUG_MSG
+        Serial.begin(9600);
+    #endif
     pinMode(fwd_button, INPUT_PULLUP);
     pinMode(rev_button, INPUT_PULLUP);
-    pinMode(STOPPER_PIN, INPUT_PULLUP);
+    pinMode(STOPPER_PIN_1, INPUT_PULLUP);
+    pinMode(STOPPER_PIN_2, INPUT_PULLUP);
     pinMode(STEP_PIN, OUTPUT);
     pinMode(DIR_PIN, OUTPUT);
     pinMode(ENABLE_PIN, OUTPUT);
 
     digitalWrite(ENABLE_PIN, HIGH);
 
-    state = digitalRead(STOPPER_PIN) ? STANDBY : REVERSE;
+    state = STANDBY;
 }
 
-void loop() {
+void loop()
+{
     readButtons();
     handleState();
-    switch (state) {
+    switch (state)
+    {
     case STANDBY:
         stopMotor();
         break;
-    case AUTO_MOVE:
-    case MANUAL_MOVE:
-        move();
+    case FORWARD:
+        if (!stopper1)
+        {
+            move();
+        } else
+        {
+            stopMotor();
+        }
         break;
-    case REVERSE:
-        reverse();
-        state = STANDBY;
+    case BACKWARD:
+        if(!stopper2)
+        {
+            move();
+        } else
+        {
+            stopMotor();
+        }
         break;
     default:
-        Serial.println("Oops");
+        #ifdef DEBUG_MSG
+            Serial.println("Oops");
+        #endif
         break;
     }
 }
 
-void move() {
+void move()
+{
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(frequency);
     digitalWrite(STEP_PIN, LOW);
     delayMicroseconds(frequency);
 }
 
-void readButtons() {
-    stopper = !digitalRead(STOPPER_PIN);
-    Serial.print("stopper:"); Serial.println(stopper);
+void readButtons()
+{
+    stopper1 = !digitalRead(STOPPER_PIN_1);
+    stopper2 = !digitalRead(STOPPER_PIN_2);
+#ifdef DEBUG_MSG
+    Serial.print("stopper1:"); Serial.println(stopper1);
+    Serial.print("stopper2:"); Serial.println(stopper2);
+#endif
+
     forwardButton = !digitalRead(fwd_button);
-    Serial.print("forwardButton:"); Serial.println(forwardButton);
     backwardButton = !digitalRead(rev_button);
+#ifdef DEBUG_MSG
+    Serial.print("forwardButton:"); Serial.println(forwardButton);
     Serial.print("backwardButton:"); Serial.println(backwardButton);
+#endif
 }
 
-void handleState() {
-    if(stopper) {
-        state = REVERSE;
-        Serial.println("REVERSE");
-        return;
+void handleState()
+{
+    if (!isMotorRunning && !stopper1 && forwardButton)
+    {
+        state = FORWARD;
+        digitalWrite(DIR_PIN, Direction::Forward);
+        isMotorRunning = true;
     }
-
-    if(forwardButton) {
-        if(!isMotorRunning) {
-            digitalWrite(ENABLE_PIN, LOW);
-            isMotorRunning = true;
-            buttonHold = millis();
-            digitalWrite(DIR_PIN, Forward);
-            state = AUTO_MOVE;
-            Serial.println("AUTO_MOVE");
-        }
-        if(isMotorRunning && (millis() - buttonHold) > holdThreshold) {
-            state = MANUAL_MOVE;
-            Serial.println("MANUAL_MOVE");
-        }
-    } else {
-        if(state == MANUAL_MOVE) {
-            state = STANDBY;
-            Serial.println("STANDBY");
-        }
+    if (!isMotorRunning && !stopper2 && backwardButton)
+    {
+        state = BACKWARD;
+        digitalWrite(DIR_PIN, Direction::Backward);
+        isMotorRunning = true;
     }
 }
 
-void stopMotor() {
-    Serial.println("stopMotor");
+void stopMotor()
+{
+    #ifdef DEBUG_MSG
+        Serial.println("stopMotor");
+    #endif
+    state = STANDBY;
     isMotorRunning = false;
     digitalWrite(ENABLE_PIN, HIGH);
-}
-
-void reverse() {
-    Serial.println("reverse start");
-    Serial.print("direction1"); Serial.println(direction);
-    direction = static_cast<Direction>(direction ^ (Forward ^ Backward));
-    Serial.print("direction2"); Serial.println(direction);
-    digitalWrite(DIR_PIN, direction);
-    for(size_t i = 0; i < REVERSE_STEPS; ++i) {
-        digitalWrite(STEP_PIN, HIGH);
-        delayMicroseconds(frequency);
-        digitalWrite(STEP_PIN, LOW);
-        delayMicroseconds(frequency);
-    }
-    Serial.print("direction3"); Serial.println(direction);
-    direction = static_cast<Direction>(direction ^ (Forward ^ Backward));
-    Serial.print("direction4"); Serial.println(direction);
-    Serial.println("reverse stop");
 }
